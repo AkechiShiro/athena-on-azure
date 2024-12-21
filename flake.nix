36d44f508957d2baa1d171dd460cad608d13f811
AkechiShiro <14914796+AkechiShiro@users.noreply.github.com>
2024-12-21 08:08:56 +0100

    add(flake.nix): runtime configuration for flake rebuild

--- flake.nix
+++ flake.nix
@@ -5,10 +5,44 @@
   outputs = { nixos, ... }:
     let
       username = "athena-azure";
+      desktop = "gnome";
+      dmanager = "gdm";
       system = "x86_64-linux";
       pkgs = import nixos { inherit system; config.allowUnfree = true; };
     in
     {
+       "runtime" = mkSystem [
+          "/etc/nixos/hardware-configuration.nix"
+          (import ./common.nix {inherit username;})
+          home-manager.nixosModules.home-manager
+          ./.
+          {
+          athena = {
+            inherit bootloader terminal theme mainShell browser;
+            enable = true;
+            baseConfiguration = true;
+            baseSoftware = true;
+            baseLocale = true;
+            homeManagerUser = username;
+            desktopManager = desktop;
+            displayManager = dmanager;
+          };
+          }
+        ];
+
+      packages.${system}.azure-image =
+        let
+          img = nixos.lib.nixosSystem {
+            inherit pkgs system;
+
+            modules = [
+              ./image.nix
+	            ./.
+              (import ./common.nix { inherit username; })
+            ];
+          };
+        in
+        img.config.system.build.azureImage;
       packages.${system}.azure-image =
         let
           img = nixos.lib.nixosSystem {
